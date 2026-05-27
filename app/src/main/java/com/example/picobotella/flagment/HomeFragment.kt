47805e883a99dad4be9a071fb97047fff77af2aa
cristian camilo cobo mendoza <ccobo1405@gmail.com>
2026-05-27 11:24:40 -0500

    Agregar menu superior del home

--- app/src/main/java/com/example/picobotella/flagment/HomeFragment.kt
+++ app/src/main/java/com/example/picobotella/flagment/HomeFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import android.view.MotionEvent
+import android.widget.ImageButton
 
 class HomeFragment : Fragment() {
 
@@ -36,6 +38,36 @@ class HomeFragment : Fragment() {
         mediaPlayer = MediaPlayer.create(requireContext(), R.raw.musica_home)
         mediaPlayer?.isLooping = true  // se repite en bucle
         mediaPlayer?.start()
+
+        val botones = listOf(
+            view.findViewById<ImageButton>(R.id.btnEstrella),
+            view.findViewById<ImageButton>(R.id.btnSonido),
+            view.findViewById<ImageButton>(R.id.btnJuego),
+            view.findViewById<ImageButton>(R.id.btnAgregar),
+            view.findViewById<ImageButton>(R.id.btnCompartir)
+        )
+
+        botones.forEach { boton ->
+            boton.setOnTouchListener { v, event ->
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        v.animate()
+                            .scaleX(0.85f)
+                            .scaleY(0.85f)
+                            .setDuration(100)
+                            .start()
+                    }
+                    MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                        v.animate()
+                            .scaleX(1f)
+                            .scaleY(1f)
+                            .setDuration(100)
+                            .start()
+                    }
+                }
+                false
+            }
+        }
     }
 
     override fun onPause() {
@@ -54,4 +86,5 @@ class HomeFragment : Fragment() {
         mediaPlayer?.release()
         mediaPlayer = null
     }
+
 }
